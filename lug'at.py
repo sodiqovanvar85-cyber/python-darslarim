f4d958d346ac8c952b79c38d6936a7c9ccb66b33
sodiqovanvar85-cyber <sodiqovanvar85@gmail.com>
2026-02-03 22:17:52 +0300

    Create lug'at.py

---
+++ lug'at.py
@@ -0,0 +1,7 @@
+en_uz = {'apple': 'olma' , 'peach' : 'shaftoli', "tomato" : 'pomidor', 'fruktis' : 'meva'}
+print(f"{en_uz['apple']} bor, {en_uz['peach']} bor")
+ismlar = ['anvar','sanjar','zohid','hoshim']
+for ism in ismlar:
+    print(ism)
+
+
